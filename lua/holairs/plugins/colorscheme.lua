b62557bd7df84e429948e94d01b27d0aa8f888eb
Holairs <bryanrosalessakil@gmail.com>
2025-02-17 18:59:51 -0600

    Float elements fixed

--- lua/holairs/plugins/colorscheme.lua
+++ lua/holairs/plugins/colorscheme.lua
@@ -8,6 +8,8 @@ return {
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 			vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 			vim.api.nvim_set_hl(0, "CursorLine", { bg = "none", underline = true })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 		end,
 	},
 }
