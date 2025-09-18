3434474b60497288c80e8ec7f47e490a2ca818e8
Holairs <bryanrosalessakil@gmail.com>
2025-09-17 21:35:15 -0600

    remobe unnecesary mason-file

--- lua/holairs/plugins/mason.lua
+++ lua/holairs/plugins/mason.lua
@@ -1,16 +1,29 @@
 -- ~/.config/nvim/lua/plugins/mason.lua
 
 return {
-	"williamboman/mason.nvim",
-	opts = {
-		registries = {
-			"github:mason-org/mason-registry",
-			"github:Crashdummyy/mason-registry",
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			registries = {
+				"github:mason-org/mason-registry",
+				"github:Crashdummyy/mason-registry",
+			},
+			ensure_installed = {
+				"roslyn",
+				"rzls",
+				"csharpier", -- csharp
+			},
 		},
-		ensure_installed = {
-			"roslyn",
-			"rzls",
-			"csharpier", -- csharp
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
 		},
+		config = function()
+			local mason_lspconfig = require("mason-lspconfig")
+			mason_lspconfig.setup({})
+		end,
 	},
 }
